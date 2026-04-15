464331373152a033b5a3e214fd69879b83cfbd83
Ishika Nadar <ishunadarishika54@gmail.com>
2026-04-15 22:47:59 +0530

    Create 680-ValidPalindromeIi

---
+++ 680-ValidPalindromeIi/680-ValidPalindromeIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/15/2026, 10:47:57 PM
+class Solution {
+public:
+bool isPalindrome(const string& s, int left, int right) { 
+    while (left < right) {
+         if (s[left] != s[right]) return false; 
+         left++;
+         right--;
+     } return true; 
+  }
+    bool validPalindrome(string s) {
+        int left =0;
+        int right=s.size()-1;
+        while(left<right){
+            if(s[left]!=s[right]){   
+        return isPalindrome(s,left+1,right)|| isPalindrome(s,left,right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
