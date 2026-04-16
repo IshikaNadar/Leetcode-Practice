eb2a30011759dddb192f2fde93c5ab4f93f064f2
Ishika Nadar <ishunadarishika54@gmail.com>
2026-04-16 10:24:15 +0530

    File updated at 4/16/2026, 10:24:15 AM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.cpp
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.cpp
@@ -1,41 +1,18 @@
-// Last updated: 4/16/2026, 10:19:12 AM
+// Last updated: 4/16/2026, 10:24:15 AM
 1class Solution {
 2public:
-3   int largestRectangleArea(vector<int>& heights) {
-4   int n = heights.size();
-5   vector<int> leftMin(n);
-6   leftMin[0] = -1;  
-7
-8
-9   stack<int> leftStack;
-10   leftStack.push(0);
-11   for (int i = 1; i < n; i++) {
-12       while (!leftStack.empty() && heights[leftStack.top()] >= heights[i]) {
-13           leftStack.pop();
-14       }
-15       leftMin[i] = leftStack.empty() ? -1 : leftStack.top();
-16       leftStack.push(i);
-17   }
-18   vector<int> rightMin(n);
-19   rightMin[n - 1] = n; 
-20
-21
-22   stack<int> rightStack;
-23   rightStack.push(n - 1);
-24
-25   for (int i = n - 2; i >= 0; i--) {
-26       while (!rightStack.empty() && heights[rightStack.top()] >= heights[i]) {
-27           rightStack.pop();
-28       }
-29       rightMin[i] = rightStack.empty() ? n : rightStack.top();
-30       rightStack.push(i);
-31   }
-32   int maxArea = 0;
-33   for (int i = 0; i < n; i++) {
-34       int width = (rightMin[i] - leftMin[i]) - 1;
-35       int area = width * heights[i];
-36       maxArea = max(maxArea, area);
-37   }
-38   return maxArea;
-39}
-40};
+3    int subarraySum(vector<int>& nums, int k) {
+4        int count=0;
+5        int n =nums.size();
+6        for(int i =0;i<n;i++){
+7           int  sum= 0;
+8            for(int j =i;j<n;j++){
+9                sum += nums[j];
+10                if(sum ==k){
+11                    count++;
+12                }
+13            }
+14        }
+15        return count;
+16    }
+17};
