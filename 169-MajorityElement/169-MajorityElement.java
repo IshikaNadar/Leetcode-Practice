e29520d54c01426b516190731c74ce8d96fcd550
Ishika Nadar <ishunadarishika54@gmail.com>
2026-06-28 22:30:41 +0530

    File updated at 6/28/2026, 10:30:52 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,7 +1,22 @@
-// Last updated: 6/28/2026, 1:13:48 PM
+// Last updated: 6/28/2026, 10:30:52 PM
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3         Arrays.sort(nums);
-4        return nums[nums.length / 2];
-5    }
-6}
+3        //better approach with sorting  T.C. = O(nlogn)
+4         Arrays.sort(nums);
+5         int n = nums.length;
+6         int freq = 1;
+7         int ans = nums[0];
+8         for(int i =1;i<n;i++){
+9          if(nums[i]== nums[i-1]){
+10            freq++;
+11          }else{
+12            freq=1;
+13            ans=nums[i];
+14          }
+15          if(freq > n/2){
+16            return ans;
+17          }
+18         }
+19         return ans;
+20    }
+21}
