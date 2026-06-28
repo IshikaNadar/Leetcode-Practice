fc8b9276d18860d455884b860d3ca09ade938197
Ishika Nadar <ishunadarishika54@gmail.com>
2026-06-28 13:13:38 +0530

    File updated at 6/28/2026, 1:13:48 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,33 +1,7 @@
-// Last updated: 4/15/2026, 10:48:12 PM
-class Solution {
-    public int majorityElement(int[] nums) {
-        /*optimise code without TLE:
-        int n = nums.length;
-        Arrays.sort(nums);
-     // Arrays.toString(nums);
-        int count =1;
-       int ans=nums[0];
-       for(int i =1;i<n;i++){
-        if(nums[i]==nums[i-1]){
-            count++;
-        }else{
-            count =1;
-            ans=nums[i];
-        }
-        if(count > n/2){return ans;}
-       }
-       return ans; */
-       int count =0,ans=0;
-       for(int i =0;i<nums.length;i++){
-        if(count==0){
-        ans =nums[i];
-        }
-        if(ans==nums[i]){
-            count++;
-        }else{
-            count--;
-        }
-       }return ans;
-
-    }
-}
+// Last updated: 6/28/2026, 1:13:48 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3         Arrays.sort(nums);
+4        return nums[nums.length / 2];
+5    }
+6}
